13b22d47855256fa49f864f61f581e8c1f012bf7
Christian Manuel Gómez Velázquez <cristian.gomez@ulv.edu.mx>
2025-11-24 17:44:30 -0600

    cambios

--- bin/practica3_fundamentosmate.dart
+++ bin/practica3_fundamentosmate.dart
@@ -3,6 +3,7 @@ import 'area_poligono.dart';
 import 'dart:io';
 
 void main(List<String> arguments) {
-  stdout.writeln('Cálculo de perímetro y área de un polígono regular');
-  stdout.write('Ingrese el número de lados del polígono: ');
+  perimetro();
+
+  
 }
