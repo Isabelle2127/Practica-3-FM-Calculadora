73a80b1edc75e26ae9c734cad2ef3fa43f3f6703
Enibian Asto <isabele.asto@ulv.edu.mx>
2025-11-24 08:38:04 -0600

    Primera parte de la calculadora

---
+++ bin/practica3_fundamentosmate.dart
@@ -0,0 +1,8 @@
+import 'perimetro.dart';
+import 'area_poligono.dart';
+import 'dart:io';
+
+void main(List<String> arguments) {
+  stdout.writeln('Cálculo de perímetro y área de un polígono regular');
+  stdout.write('Ingrese el número de lados del polígono: ');
+}
